0bddc97234bab1f303be6a4b5b1936156bd7e3c6
Edgar V <evenegas9@ucmerced.edu>
2019-09-08 23:35:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+def prime? integer
+  return false if integer < 2
+
+  # solution takes O(n)
+  (2..integer-1).to_a().each do |num|
+    if integer % num == 0
+      return false
+    end
+  end
+  return true
+end
+
+#11 => true
+#19 => true
+#4  => false
+
+
+#[2, 3, 4, 5, 6, 7, 8, 9]
+# 25 => false
