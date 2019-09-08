4f2bc43bff300b9796be5edb4fb2ea2c288ef360
Edgar V <evenegas9@ucmerced.edu>
2019-09-08 23:15:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime? integer
+  nums = (2..9).to_a
+
+  nums.all do |num|
+    puts num
+    num != integer && integer % num == 0
+  end
+end
+
+#11 => true
+#19 => true
+#4  => false
